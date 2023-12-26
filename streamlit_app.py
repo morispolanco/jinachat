d5ac915c5a3ff8284f78ab8d254168c636cacee1
morispolanco <morispolanco@gmail.com>
2023-12-26 05:00:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,23 @@
 import streamlit as st
-import requests
+import claude2
 
-# Función para realizar la solicitud a la API
-def obtener_precio_mas_bajo(nombre_producto):
-    url_api = "https://api.chat.jina.ai/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer 8eu3RBqG1GHlubaZwW4k:d16d7aaf71ae7b1f7ffcb2d009ba1df36b6132d85a6870c75b0ca8737db73edb",
-    }
+st.title("Buscador de precios en Guatemala")
 
-    payload = {
-        "messages": [{"role": "user", "content": f"Buscar el precio más bajo de {nombre_producto} en Guatemala"}]
-    }
+product = st.text_input("Ingrese el producto a buscar")
 
-    response = requests.post(url_api, headers=headers, json=payload)
+if product:
+    with st.spinner("Buscando precios..."):
+        search_query = f"{product} precio más bajo Guatemala"
+        search_results = claude2.search(search_query, sources=["web"])
 
-    if response.status_code == 200:
-        return response.json()["completions"][0]["content"]
-    else:
-        return f"Error: {response.status_code}"
+    min_price = None
+    min_price_source = None
+    for result in search_results["search_results"]:
+        if result.get("price") and (not min_price or result["price"] < min_price):
+            min_price = result["price"]  
+            min_price_source = result["source"]
 
-# Configuración de la interfaz de Streamlit
-st.title("Buscador de Precio Más Bajo en Guatemala")
-
-# Obtener el nombre del producto del usuario
-nombre_producto = st.text_input("Ingrese el nombre del producto:")
-if nombre_producto:
-    # Obtener el precio más bajo utilizando la API
-    precio_mas_bajo = obtener_precio_mas_bajo(nombre_producto)
-
-    # Mostrar el resultado en la interfaz
-    st.success(f"El precio más bajo de {nombre_producto} en Guatemala es: {precio_mas_bajo}")
+    if min_price:
+        st.success(f"El precio más bajo encontrado de {product} es {min_price} en {min_price_source}")
+    else: 
+        st.error("No se encontraron resultados con precio para este producto")
