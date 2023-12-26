63864622d34e5e9c28289b61db5392eeac022bcf
morispolanco <morispolanco@gmail.com>
2023-12-26 04:05:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,33 @@
 import streamlit as st
 import requests
 
-def obtener_precio_mas_bajo(nombre_producto):
-    url_api = "https://api.chat.jina.ai/v1/chat/completions"
+# Función para realizar la solicitud a la API
+def get_lowest_price(product_name):
+    api_url = "https://api.chat.jina.ai/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer 8eu3RBqG1GHlubaZwW4k:d16d7aaf71ae7b1f7ffcb2d009ba1df36b6132d85a6870c75b0ca8737db73edb",
     }
 
     payload = {
-        "messages": [
-            {
-                "role": "user",
-                "content": f"Buscar precio más bajo de {nombre_producto} en Guatemala"
-            }
-        ]
+        "messages": [{"role": "user", "content": f"Find the lowest price of {product_name}"}]
     }
 
-    response = requests.post(url_api, headers=headers, json=payload)
+    response = requests.post(api_url, headers=headers, json=payload)
 
     if response.status_code == 200:
-        result = response.json()
-        return result["completions"][0]["content"]
+        return response.json()["completions"][0]["content"]
     else:
         return f"Error: {response.status_code}"
 
 # Configuración de la interfaz de Streamlit
-st.title("Buscador de Precio Más Bajo en Guatemala")
+st.title("Lowest Price Finder")
 
 # Obtener el nombre del producto del usuario
-nombre_producto = st.text_input("Ingrese el nombre del producto:")
-if nombre_producto:
+product_name = st.text_input("Enter the product name:")
+if product_name:
     # Obtener el precio más bajo utilizando la API
-    precio_mas_bajo = obtener_precio_mas_bajo(nombre_producto)
+    lowest_price = get_lowest_price(product_name)
 
     # Mostrar el resultado en la interfaz
-    st.success(f"El precio más bajo de {nombre_producto} en Guatemala es: {precio_mas_bajo}")
+    st.success(f"The lowest price of {product_name} is: {lowest_price}")
