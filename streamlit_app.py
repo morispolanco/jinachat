ba21cb02988b03298e883c65903323bb5444644c
morispolanco <morispolanco@gmail.com>
2023-12-26 04:21:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,28 @@
 import streamlit as st
 import requests
+import json
 
-# Define the API endpoint and authorization header
-api_endpoint = "https://api.chat.jina.ai/v1/chat/completions"
+# Configura las credenciales de la API
+api_url = "https://api.chat.jina.ai/v1/chat/completions"
 headers = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer 8eu3RBqG1GHlubaZwW4k:d16d7aaf71ae7b1f7ffcb2d009ba1df36b6132d85a6870c75b0ca8737db73edb",
+    "Authorization": "Bearer 8eu3RBqG1GHlubaZwW4k:d16d7aaf71ae7b1f7ffcb2d009ba1df36b6132d85a6870c75b0ca8737db73edb"
 }
 
-# Create a Streamlit app
-st.title("Guatemala Product Price Finder")
-product_name = st.text_input("Enter the name of the product you want to find the price of:")
-
-# Send a request to the API with the product name
-if st.button("Find Price"):
+# Función que envía una solicitud a la API y devuelve el resultado
+def get_prices(product_name):
     data = {
         "messages": [
             {
                 "role": "user",
-                "content": f"What is the lowest price of {product_name} in Guatemala?",
+                "content": f"What is the lowest price for {product_name} in Guatemala?"
             }
         ]
     }
-    response = requests.post(api_endpoint, headers=headers, json=data)
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    response.raise_for_status()
+    return response.json()
 
-    # Extract the price from the API response
-    if response.status_code == 200:
-        result = response.json()["completions"][0]["content"]
-        price = float(result.split(" ")[2])
-        st.write(f"The lowest price of {product_name} in Guatemala is {price} {result.split(' ')[3]}")
-    else:
-        st.error("Sorry, there was an error getting the price. Please try again.")
+# Crea una interfaz de usuario para el usuario que permite ingresar el nombre del producto
+st.title("Precio más bajo de un producto en Guatemala")
+product_name = st.text_input
