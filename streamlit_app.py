d92060c03b2b267f7f281561c0f4e44e66d83257
morispolanco <morispolanco@gmail.com>
2023-12-26 04:25:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,33 @@
 import streamlit as st
 import requests
-import json
 
-# Configura las credenciales de la API
-api_url = "https://api.chat.jina.ai/v1/chat/completions"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer 8eu3RBqG1GHlubaZwW4k:d16d7aaf71ae7b1f7ffcb2d009ba1df36b6132d85a6870c75b0ca8737db73edb"
-}
-
-# Función que envía una solicitud a la API y devuelve el resultado
-def get_prices(product_name):
-    data = {
-        "messages": [
-            {
-                "role": "user",
-                "content": f"What is the lowest price for {product_name} in Guatemala?"
-            }
-        ]
+# Función para realizar la solicitud a la API
+def obtener_precio_mas_bajo(nombre_producto):
+    url_api = "https://api.chat.jina.ai/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 8eu3RBqG1GHlubaZwW4k:d16d7aaf71ae7b1f7ffcb2d009ba1df36b6132d85a6870c75b0ca8737db73edb",
     }
-    response = requests.post(api_url, headers=headers, data=json.dumps(data))
-    response.raise_for_status()
-    return response.json()
 
-# Crea una interfaz de usuario para el usuario que permite ingresar el nombre del producto
-st.title("Precio más bajo de un producto en Guatemala")
-product_name = st.text_input
+    payload = {
+        "messages": [{"role": "user", "content": f"Buscar el precio más bajo de {nombre_producto} en Guatemala"}]
+    }
+
+    response = requests.post(url_api, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        return response.json()["completions"][0]["content"]
+    else:
+        return f"Error: {response.status_code}"
+
+# Configuración de la interfaz de Streamlit
+st.title("Buscador de Precio Más Bajo en Guatemala")
+
+# Obtener el nombre del producto del usuario
+nombre_producto = st.text_input("Ingrese el nombre del producto:")
+if nombre_producto:
+    # Obtener el precio más bajo utilizando la API
+    precio_mas_bajo = obtener_precio_mas_bajo(nombre_producto)
+
+    # Mostrar el resultado en la interfaz
+    st.success(f"El precio más bajo de {nombre_producto} en Guatemala es: {precio_mas_bajo}")
